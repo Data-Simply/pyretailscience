a22ae98bd5e564e282500dbd3c5aae650069b9fb
Vas <ucabvp1@ucl.ac.uk>
2024-09-27 11:07:35 +0300

    chore: move grid lines behind plot

--- pyretailscience/style/graph_utils.py
+++ pyretailscience/style/graph_utils.py
@@ -87,7 +87,8 @@ def standard_graph_styles(
     Returns:
         Axes: The graph with the styles applied.
     """
-    ax.set_facecolor("w")
+    ax.set_facecolor("w")  # set background colour to white
+    ax.set_axisbelow(True)  # set grid lines behind the plot
     ax.spines[["top", "right"]].set_visible(False)
     ax.grid(which="major", axis="x", color="#DAD8D7", alpha=0.5, zorder=1)
     ax.grid(which="major", axis="y", color="#DAD8D7", alpha=0.5, zorder=1)
